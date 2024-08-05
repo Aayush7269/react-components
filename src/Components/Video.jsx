fd2da4c3d67601cc51200723164b0016a60b1982
Aayush Sharma <saayush272@gmail.com>
2024-08-05 21:07:30 +1000

    UseState

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Video() {
+
+ const [val,setVal] = useState({name:'aayush', isBanned:false})
+
+const[score,setScore] = useState(0);
+
+ 
+
   return (
-    <div>Video</div>
+  <>
+  
+    <div className='p-4'>
+      <h1>name : {val.name}</h1>
+      <h1>name : {val.isBanned.toString()}</h1>
+      <button onClick={()=>setVal(()=>({...val, isBanned:!val.isBanned}))} className='bg-blue-400 rounded-full px-2 '>
+        change
+      </button>
+      </div>
+       <div className='p-4'>
+       <h1>Score : {score}</h1>
+        <button onClick={()=>setScore(prev=>prev+1)}className='bg-blue-400 rounded-full px-2 '>
+         change
+       </button>
+       </div>
+       </>
   )
 }
 
