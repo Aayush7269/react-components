e12e29dbd76e4d53c8d7b1e81e479fe3fa1b335b
Aayush Sharma <saayush272@gmail.com>
2024-08-06 03:30:57 +1000

    comments

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -13,14 +13,14 @@ const[score,setScore] = useState(0);
   
     <div className='p-4'>
       <h1>name : {val.name}</h1>
-      <h1>name : {val.isBanned.toString()}</h1>
-      <button onClick={()=>setVal(()=>({...val, isBanned:!val.isBanned}))} className='bg-blue-400 rounded-full px-2 '>
+      <h1>name : {val.isBanned.toString()}</h1> //changed the boolean to string for better visibility
+      <button onClick={()=>setVal(()=>({...val, isBanned:!val.isBanned}))} className='bg-blue-400 rounded-full px-2 '> //onclick receives a function that returns a new object with the change value of isBanned by using spread operator.
         change
       </button>
       </div>
        <div className='p-4'>
        <h1>Score : {score}</h1>
-        <button onClick={()=>setScore(prev=>prev+1)}className='bg-blue-400 rounded-full px-2 '>
+        <button onClick={()=>setScore(prev=>prev+1)} className='bg-blue-400 rounded-full px-2 '>// inside the setScore function, prev is the current score. So we increment it by 1. we always get prev as an argument and return it with the incremented value.//
          change
        </button>
        </div>
